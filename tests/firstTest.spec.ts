0350312db17eb45215e3caf21779a15110e516e0
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-12-30 00:07:26 +0530

    Create Locator Syntax Rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,35 +1,36 @@
 import { test } from "@playwright/test";
 
-// test.beforeAll(() => {
-// });
-
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:4200/");
+    await page.getByTitle("Forms").click();
+    await page.getByText("Form layouts").click();
 });
 
-test.describe("suite1", () => {
-    test.beforeEach(async ({ page }) => {
-        await page.getByTitle("Forms").click();
-    });
+test('Locator syntax rules', async ({ page }) => {
+    // By tag name
+    // await page.locator('input').first().click();
 
-    test("the first test", async ({ page }) => {
-        await page.getByText("Form layouts").click();
-        // await page.getByPlaceholder('Jane Doe').fill('Kaukau');
-    });
+    // By ID
+    await page.locator('#inputEmail1').click();
 
-    test("navigate to datepicker page", async ({ page }) => {
-        await page.getByText("Datepicker").click();
-    });
-});
+    // By class value
+    page.locator('.shape-rectangle');
 
-test.describe("suite2", () => {
-    test("the first test1", async ({ page }) => {
-        await page.getByText("Forms").click();
-        await page.getByText("Form layouts").click();
-        await page.getByPlaceholder("Jane Doe").fill("Kaukau");
-    });
+    // By attribute
+    page.locator('[placeholder="Email"]');
 
-    test("navigate to charts", async ({ page }) => {
-        await page.getByTitle("Charts").first().click();
-    });
+    // By full class value
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+
+    // Combining selectors
+    page.locator('input[placeholder="Email"][nbinput]');
+
+    // By XPath (not recommended)
+    page.locator('//*[@id="inputEmail1"]');
+
+    // By partial text match
+    page.locator(':text("Using")');
+
+    // By exact text match
+    page.locator(':text-is("Using the Grid")');
 });
