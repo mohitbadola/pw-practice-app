dfbc3c5716f18c2697aafdaf56e4367e7b81228b
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-12-31 11:58:40 +0530

    Create checkboxes test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -44,3 +44,27 @@ test.describe("Form Layouts page", () => {
     
   })
 })
+
+test('checkboxes', async({page})=>{
+    await page.getByText("Modal & Overlays").click()
+    await page.getByText("Toastr").click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+    
+
+    //to check all boxes
+    // const allBoxes = page.getByRole('checkbox')
+    // for(const box of await allBoxes.all()){
+    //     await box.check({force: true})
+    //     expect(await box.isChecked()).toBeTruthy
+    // }
+
+
+    //to uncheck all boxes
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+})
